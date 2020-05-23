ae35d441e29998668c01f807a7eaf708ef92244c
Mohammed Boudad <19875946+matrixersp@users.noreply.github.com>
2020-05-23 14:07:37 +0100

    Add dark mode and responsiveness

--- social-media-dashboard/script.js
+++ social-media-dashboard/script.js
@@ -1,1 +1,10 @@
-document.documentElement.setAttribute("data-theme", "dark");
+const toggle = document.getElementById('switch');
+
+toggle.addEventListener('change', function(event) {
+  let root = document.documentElement;
+  if(!root.getAttribute('data-theme')) {
+    root.setAttribute("data-theme", "dark");
+  } else {
+    root.removeAttribute("data-theme");
+  }
+})
