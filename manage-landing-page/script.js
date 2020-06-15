4f77ddc2ea79d0f2372b3b970c37fa19c9dad740
Mohammed Boudad <19875946+matrixersp@users.noreply.github.com>
2020-06-15 23:27:15 +0100

    layout testimonials, get-started and footer

--- manage-landing-page/script.js
+++ manage-landing-page/script.js
@@ -1,7 +1,37 @@
-var toggle = document.getElementById('menu-toggle');
-var nav = document.getElementById('main-nav');
+var toggle = document.getElementById("menu-toggle");
+var nav = document.getElementById("main-nav");
 
-toggle.addEventListener('click', function(event) {
-  nav.classList.toggle('is-open');
-  document.body.classList.toggle('noscroll');
+toggle.addEventListener("click", function (event) {
+  nav.classList.toggle("is-open");
+  document.body.classList.toggle("noscroll");
+});
+
+var list = document.getElementById("testimonials-list");
+
+let interval = null;
+list.addEventListener("mouseover", function (event) {
+  if (300 > event.clientX) {
+    clearInterval(interval);
+    interval = setInterval(scrollLeft.bind(this), 15);
+  }
+  if (window.innerWidth - 300 < event.clientX) {
+    clearInterval(interval);
+    interval = setInterval(scrollRight.bind(this), 15);
+  }
+});
+
+function scrollLeft() {
+  this.scrollLeft += -5;
+}
+
+function scrollRight() {
+  this.scrollLeft += +5;
+}
+
+list.addEventListener("mouseout", function (event) {
+  var e = event.toElement || event.relatedTarget;
+  if (e.parentNode == this || e == this || e.parentNode.parentNode == this)
+    return;
+
+  clearInterval(interval);
 });
